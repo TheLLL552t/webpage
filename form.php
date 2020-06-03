c37cbceb7649e54392243c82a880149eeb7410e4
TheLLL552t <66371655+TheLLL552t@users.noreply.github.com>
2020-06-03 09:50:28 -0400

    Add files via upload

---
+++ form.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+<?php
+
+$name = $_POST;
+echo $name;
+?>
+</body>
+</html
---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>Quiz</title>
+    </head>
+    <body>
+        <h1>Welcome to the <em><strong>QUIZ</strong></em></h1>
+        <form action="form.php" process="post">
+            <input type="text">
+            <input type="submit">
+        </form>
+
+    </body>
+</html>
